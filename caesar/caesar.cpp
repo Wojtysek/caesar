f0dd4f31ff9628ab1ac4272ba2199aca4c3f992a
Wojtysek <vojtahr1@post.cz>
2022-06-13 14:18:58 +0200

    funguje

--- caesar/caesar.cpp
+++ caesar/caesar.cpp
@@ -1,13 +1,27 @@
 ﻿#include <stdio.h>
+#include <string.h>
 
+char odhackuj(char znak) {
+
+	char mhacky[] = "áčďéěňóřšťůúýíž";
+	char mnohac[] = "acdeenorstuuyiz";
+	char vhacky[] = "ÁČĎÉĚŇÓŘŠŤŮÚÝÍŽ";
+	char vnohac[] = "ACDEENORSRUUYIZ";
+	for (int i = 0; i<strlen(mhacky); i++) {
+		if (znak == mhacky[i]) znak = mnohac[i];
+		if (znak == vhacky[i])  znak = vnohac[i];
+	}
+	return znak;
+}
 int main(void) {
-
-	char vstup[] = "peta je blbecek"; //caesarova šifra
+	
+	char vstup[] = "uwtrs úwťřš"; //caesarova šifra 
 	int posun = 3;
 
 	printf("puvodni zprava: %s\n", vstup);
 
 	for (int i = 0; vstup[i] != '\0'; i++) {
+		vstup[i] = odhackuj(vstup[i]);
 		if (vstup[i] >= 'a' && vstup[i] <= 'w' || vstup[i] >= 'A' && vstup[i] <= 'W') {
 			vstup[i] += posun;
 		}
