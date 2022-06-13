ad491a14d77689c5fb7ef49f0373de60e13a74a9
Wojtysek <vojtahr1@post.cz>
2022-06-13 13:26:16 +0200

    Přidejte soubory projektu.

---
+++ caesar/caesar.cpp
@@ -0,0 +1,19 @@
+﻿#include <stdio.h>
+
+int main(void) {
+
+	char vstup[] = "peta je blbecek"; //caesarova šifra
+	int posun = 3;
+
+	printf("puvodni zprava: %s\n", vstup);
+
+	for (int i = 0; vstup[i] != '\0'; i++) {
+		if (vstup[i] >= 'a' && vstup[i] <= 'w' || vstup[i] >= 'A' && vstup[i] <= 'W') {
+			vstup[i] += posun;
+		}
+		else if (vstup[i] >= 'x' && vstup[i] <= 'z' || vstup[i] >= 'z' && vstup[i] <= 'Z') {
+			vstup[i] += posun - 26;
+		}
+	}
+	printf("zakodovana zprava: %s\n", vstup);
+}
